c7089e2e4dc2bf136358f07d64f09d9fe7d30feb
jfgudk <j.gudknecht@elsevier.com>
2022-07-14 17:14:20 -0400

    Change greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -5,6 +5,6 @@ import "fmt"
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome to XTechWare Kubernetes Tools!", name)
+	message := fmt.Sprintf("Hello, %v. Welcome to XTechWare Kubernetes Tools!", name)
 	return message
 }
